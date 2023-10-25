a3d5132990488c82bc3b1686fb47a1822ae5d130
Sandun666 <e17168@eng.pdn.ac.lk>
2023-10-26 01:37:11 +0530

    com

--- code/complementer/complementer.v
+++ code/complementer/complementer.v
@@ -1,10 +1,11 @@
 module complementer (
     input [31:0] IN,
+    input COMP_SEL_IDEX,
     output [31:0] OUT
 );
 
-wire [31:0] NOTOUT;
-assign NOTOUT = IN^32'b11111111111111111111111111111111;
-assign OUT = NOTOUT + 1;
+    wire [31:0] NOTOUT;
+    assign NOTOUT = IN^32'b11111111111111111111111111111111;
+    assign OUT = COMP_SEL_IDEX ? NOTOUT + 1:IN;
     
 endmodule
